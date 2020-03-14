a30f3542be9ec48e44d85d35d952be63c603ce9d
Sandro Marques <sandro123iv@gmail.com>
2020-03-14 00:52:32 +0000

    Google sign-in

--- App.js
+++ App.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+import AuthScreen from './components/AuthScreen';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app.</Text>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<Text>Open up App.js to start working on your app.</Text>
+			<AuthScreen />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
+
+
+// Initialize Firebase
+const firebaseConfig = {
+	projectId: "foodie-daam",
+	apiKey: "AIzaSyB1JoA88dbyHJP5r47AxAE8ZEtJzvi0b6E",
+	authDomain: "foodie-daam.firebaseapp.com",
+	databaseURL: "https://foodie-daam.firebaseio.com",
+	storageBucket: "foodie-daam.appspot.com"
+};
+
+firebase.initializeApp(firebaseConfig);
+//firebase.analytics();
